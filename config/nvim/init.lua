344c08ee9281d562658575ee8f9871ac8a85b3f1
laczfy001 <98620859+laczfy001@users.noreply.github.com>
2025-12-25 01:07:00 +0100

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,5 @@
 -- init.lua
 
----------------------------
--- 1️⃣ Alapvető beállítások
----------------------------
 vim.o.termguicolors = true       -- 24-bit színek
 vim.o.number = true               -- sor számok
 vim.o.relativenumber = true       -- relatív sor számok
@@ -12,9 +9,6 @@ vim.o.tabstop = 2
 vim.o.smartindent = true
 vim.o.cursorline = true
 
----------------------------
--- 2️⃣ Packer telepítése
----------------------------
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
@@ -35,24 +29,18 @@ end
 
 local packer_bootstrap = ensure_packer()
 
----------------------------
--- 3️⃣ Pluginok
----------------------------
 require("packer").startup(function(use)
-  -- Packer önmaga
   use "wbthomason/packer.nvim"
 
-  -- Tokyonight Theme
   use({
     "folke/tokyonight.nvim",
     config = function()
-      -- Tokyonight beállítások
       vim.g.tokyonight_style = "storm"          -- storm, night, day
       vim.g.tokyonight_italic_comments = true
       vim.g.tokyonight_transparent = false
       vim.g.tokyonight_enable_italic = true
 
-      -- Színséma aktiválása
+
       vim.cmd("colorscheme tokyonight-storm")
     end
   })
@@ -61,9 +49,3 @@ require("packer").startup(function(use)
     require("packer").sync()
   end
 end)
-
----------------------------
--- 4️⃣ Extra tipp root fájlok mentéséhez
----------------------------
--- Ha root jog kell egy fájl mentéséhez:
--- :w !sudo tee %
