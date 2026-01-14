0a9d70788107715033830d3cadd989963909bbc9
laczfy001 <laczfy001@live.com>
2026-01-14 01:38:47 +0100

    update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -141,18 +141,19 @@
   powerManagement.enable = true;
   services.power-profiles-daemon.enable = false;
   services.thermald.enable = true;
-  
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-  battery = {
-     governor = "powersave";
-     turbo = "never";
-  };
-  charger = {
-     governor = "performance";
-     turbo = "auto";
-   };
-  };
+
+  #services.auto-cpufreq.enable = true;
+  #services.auto-cpufreq.settings = {
+  #battery = {
+  #   governor = "powersave";
+  #   turbo = "auto";
+  #};
+  #charger = {
+  #   governor = "performance";
+  #   turbo = "auto";
+  # };
+  #};
+
 
   # SSD
   services.fstrim.enable = true;
